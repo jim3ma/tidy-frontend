341e66ff8752e6ccc530911acc4d71ef2cdaec8f
Jim Mar <majinjing3@gmail.com>
2016-06-08 09:27:23 +0800

    add code for prevent html inject

---
+++ src/static/js/htmlescape.js
@@ -0,0 +1,24 @@
+/**
+ * Created by jim on 6/8/16.
+ */
+
+// http://stackoverflow.com/questions/1219860/html-encoding-in-javascript-jquery
+function htmlEscape(str) {
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/\//g, '&#x2F;');
+}
+
+function htmlUnescape(value){
+    return String(value)
+        .replace(/&quot;/g, '"')
+        .replace(/&#39;/g, "'")
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&amp;/g, '&')
+        .replace(/&#x2F;/g, '/');
+}
